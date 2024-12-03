b9ba7a716e2f6409422acd68d23f17446a91218e
Mael Lacour <public.dev@maellacour.com>
2024-12-03 16:22:15 +0100

    Day03: Solve part 2

--- AdventOfCode/Day03.cs
+++ AdventOfCode/Day03.cs
@@ -33,7 +33,42 @@ public class Day03 : BaseDay
     }
 
 
-    public override ValueTask<string> Solve_2() => throw new NotImplementedException();
+    /// <summary>
+    /// Same but with do and don't
+    /// </summary>
+    /// <returns></returns>
+    public override ValueTask<string> Solve_2()
+    {
+        int result = 0;
+        string pattern = @"mul\((?<num1>\d+),(?<num2>\d+)\)|do\(\)|don't\(\)";
+        Regex regex = new(pattern);
+        MatchCollection matches = regex.Matches(_input);
+
+        bool flag = true;
+
+        foreach (Match match in matches)
+        {
+            if (match.Value == "do()")
+            {
+                flag = true;
+            }
+            else if (match.Value == "don't()")
+            {
+                flag = false;
+            }
+            else
+            {
+                if (flag)
+                {
+                    int num1 = int.Parse(match.Groups["num1"].Value);
+                    int num2 = int.Parse(match.Groups["num2"].Value);
+                    result += num1 * num2;
+                }
+            }
+        }
+
+        return new ValueTask<string>(result.ToString());
+    }
 
     private void ParseInput()
     {
