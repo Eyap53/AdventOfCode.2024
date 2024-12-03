3064f80453238702d9aba8b42cb3e96ee6910bd7
Mael Lacour <public.dev@maellacour.com>
2024-12-03 11:49:28 +0100

    Add Day 01 implementation with input parsing and solution methods

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -1,15 +1,61 @@
-﻿namespace AdventOfCode;
+﻿using System.Collections.Generic;
+
+namespace AdventOfCode;
 
 public class Day01 : BaseDay
 {
-    private readonly string _input;
+    private List<int> _list1;
+    private List<int> _list2;
 
     public Day01()
     {
-        _input = File.ReadAllText(InputFilePath);
+        ParseInput();
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    public override ValueTask<string> Solve_1()
+    {
+        _list1.Sort();
+        _list2.Sort();
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+        int distance = 0;
+
+        for (int i = 0; i < _list1.Count; i++)
+        {
+            distance += Math.Abs(_list1[i] - _list2[i]);
+        }
+
+        return new(distance.ToString());
+    }
+
+    /// <summary>
+    /// Not Optimized.
+    /// </summary>
+    public override ValueTask<string> Solve_2()
+    {
+        int similarityScore = 0;
+
+        for (int i = 0; i < _list1.Count; i++)
+        {
+            similarityScore += _list1[i] * _list2.Count(x => x == _list1[i]);
+        }
+
+        return new(similarityScore.ToString());
+    }
+
+    private void ParseInput()
+    {
+        string[] input = File.ReadAllLines(InputFilePath);
+        _list1 = new();
+        _list2 = new();
+
+        foreach (var line in input)
+        {
+            string[] lineParts = line.Split("   ");
+            if (lineParts.Length == 2 && int.TryParse(lineParts[0], out int num1) && int.TryParse(lineParts[1], out int num2))
+            {
+                _list1.Add(num1);
+                _list2.Add(num2);
+            }
+        }
+    }
 }
